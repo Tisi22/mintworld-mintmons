feca48b06b0a21850e7e5af8308e62d545d79474
Tisi22 <teresa@ngmi.ai>
2023-06-22 15:18:56 +0200

    update

--- contracts/Items.sol
+++ contracts/Items.sol
@@ -12,7 +12,7 @@ interface MintWorldToken {
     function balanceOf(address account) external view returns (uint256);
 }
 
-contract MyToken is ERC1155, Ownable, ERC1155Burnable {
+contract Items is ERC1155, Ownable, ERC1155Burnable {
 
     MintWorldToken mwgContract;
 
@@ -44,8 +44,48 @@ contract MyToken is ERC1155, Ownable, ERC1155Burnable {
         require(mwgContract.balanceOf(msg.sender) >= valueMWG, "Not enough MWG");
         require(prices[id]*amount >= valueMWG, "Not enough MWG sent");
 
-        mwgContract.transferFrom(msg.sender, usedMWG ,Math.mul(valueMWG, uint256(10)**mwgContract.decimals()));
+        // Transfer MWG tokens from the sender to usedMWG address
+        require(
+        mwgContract.transferFrom(
+            msg.sender,
+            usedMWG,
+            Math.mul(valueMWG, 10**uint256(mwgContract.decimals()))
+        ),
+            "Failed to transfer MWG tokens"
+        );
+
         _mint(msg.sender, id, amount, "");
+    }
+
+    function mintAndBurn(uint256 id, uint256 amount, uint256 valueMWG, uint256 amountToBurn) public 
+    {
+        require(mwgContract.balanceOf(msg.sender) >= valueMWG, "Not enough MWG");
+        require(prices[id]*amount >= valueMWG, "Not enough MWG sent");
+
+        // Transfer MWG tokens from the sender to usedMWG address
+        require(
+        mwgContract.transferFrom(
+            msg.sender,
+            usedMWG,
+            Math.mul(valueMWG, 10**uint256(mwgContract.decimals()))
+        ),
+            "Failed to transfer MWG tokens"
+        );
+
+        burn(msg.sender, id, amountToBurn);
+
+        _mint(msg.sender, id, amount, "");
+    }
+
+    function burnItems(uint256[] calldata ids, uint256[] calldata amounts) public
+    {
+        require(ids.length == amounts.length, "Invalid arguments");
+
+        for (uint i = 0; i < ids.length; i++)
+        {
+            burn(msg.sender, ids[i], amounts[i]);
+        }
+        
     }
 
 }
